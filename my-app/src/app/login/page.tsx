f6e84c2698473a0a086123c29477bf53d5299bed
Sagnik Ghosh <52010932+sagnik26@users.noreply.github.com>
2023-12-16 03:15:58 +0530

    NextJs Full Stack App setup done

---
+++ my-app/src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className='text-center text-white text-2xl'>Login</h1>
+    </div>
+  )
+}
+
+export default page
---
+++ my-app/src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        <h1 className='text-center text-white'>Signup</h1>
+    </div>
+  )
+}
+
+export default page
